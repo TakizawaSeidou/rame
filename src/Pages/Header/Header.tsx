cbe410a1166140e39c8ad996a0f44e18fcbf7733
user <zumaevanvar36@gmail.com>
2023-06-10 18:10:56 +0300

    asd

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -5,6 +5,7 @@ function Menu(){
     return(
         <div className={style.container}>
             TaskPro
+            <img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKNs2vn_UjsrFRuW8oni58ACsx97chKNfJqBs0pqnBzvwyNLz-'} className={style.men}/>
         </div>
 
     )
