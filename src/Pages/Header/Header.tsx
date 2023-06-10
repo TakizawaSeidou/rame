de55326ce9a503e8f05d3993e5592fbbeb17d2d3
user <zumaevanvar36@gmail.com>
2023-06-10 16:09:35 +0300

    asd

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -5,7 +5,9 @@ import style from './style.module.css'
 function Menu(){
     return(
         <div className={style.container}>
-
+            <div className={style.menu}> <img src={'https://www.iphones.ru/wp-content/uploads/2022/10/916895BF-4B7B-4D45-A22F-9D806094C40A.jpeg'}/>Task Pro
+            </div>
+            <div className={style.menub}></div>
         </div>
 
     )
