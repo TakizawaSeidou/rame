4a1780d5ca08fef9b82b67628017ee3e9e2e28b6
user <zumaevanvar36@gmail.com>
2023-06-10 19:16:26 +0300

    asd

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import style from './style.module.css'
+import diamond from '../../resourse/diamond.jpeg'
 
 
 function Menu(){
     return(
         <div className={style.rav}>
         <div className={style.container}>
-            Subscribe
+            <div>Subscribe</div>
+
+            <img src={diamond} className={style.hz}/>
             <br/>
             To Premium
+            <br className={style.mem}/>
+            Buy premium membership
+
 
         </div>
+
 </div>
     )
 }
