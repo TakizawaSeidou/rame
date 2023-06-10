fd4e61e5ba02c8b0cf8c18db26bfd185f8a70151
user <zumaevanvar36@gmail.com>
2023-06-10 18:47:01 +0300

    asd

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -4,10 +4,11 @@ import style from './style.module.css'
 
 function Menu(){
     return(
+        <div className={style.rav}>
         <div className={style.container}>
 adad
         </div>
-
+</div>
     )
 }
 export default Menu;
