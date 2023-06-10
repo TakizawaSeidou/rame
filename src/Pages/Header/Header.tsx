f51f1dc28063cdbdf2f5b631cee5998e29a50d50
user <zumaevanvar36@gmail.com>
2023-06-10 18:07:44 +0300

    asd

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import style from './style.module.css'
 
-
 function Menu(){
     return(
         <div className={style.container}>
-            <div className={style.menu}>Task Pro
-                <img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKNs2vn_UjsrFRuW8oni58ACsx97chKNfJqBs0pqnBzvwyNLz-'} className={style.men}/>
-            </div>
-
+            TaskPro
         </div>
 
     )
