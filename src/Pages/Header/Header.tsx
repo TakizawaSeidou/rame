a8a453a9b8f403d9b5d498cf43b9a438fcabf715
user <zumaevanvar36@gmail.com>
2023-06-10 18:31:31 +0300

    asd

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import style from './style.module.css'
+import tri from '../../resourse/tri.png'
+
 
 function Menu(){
     return(
         <div className={style.container}>
             TaskPro
-            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_591aDUhOqM-mUfxquHVnamWcq5yQN-8tYq0xN_g19afHLi1a'} className={style.men}/>
+            <img src={tri} className={style.men}/>
         </div>
 
     )
