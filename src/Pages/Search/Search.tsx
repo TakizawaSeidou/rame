5f6e821252ee970bf2f17e6a9790045e0d4af590
user <zumaevanvar36@gmail.com>
2023-06-13 14:22:22 +0300

    asd

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import style from './style.module.css'
-import diamond from '../../resourse/diamond.jpeg'
+import lupa from '../../resourse/lupa.png'
+import menu from '../../resourse/menu.png'
 
 
 function Search(){
     return(
         <div className={style.container}>
-            <img src={'https://e7.pngegg.com/pngimages/409/527/png-clipart-magnifying-glass-computer-icons-magnifying-glass-glass-black.png'} className={style.ser}/>
+            <img src={lupa} className={style.ser}/>
             <input placeholder={'Search  Anything'} className={style.int}/>
-            \<img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRh-eALKjfQWuZA7z4hxKKNlx2ZswSrIaeUrSywxstBc5mHrVti'} className={style.ser}/>
+            <img src={menu} className={style.ser}/>
 
         </div>
     )
