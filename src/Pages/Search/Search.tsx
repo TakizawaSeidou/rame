abf2ff7783d036ea6c6a5875fec812461766d89e
user <zumaevanvar36@gmail.com>
2023-06-13 14:05:07 +0300

    asd

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -6,8 +6,9 @@ import diamond from '../../resourse/diamond.jpeg'
 function Search(){
     return(
         <div className={style.container}>
+            <img src={'https://e7.pngegg.com/pngimages/409/527/png-clipart-magnifying-glass-computer-icons-magnifying-glass-glass-black.png'} className={style.ser}/>
             <input placeholder={'Search  Anything'} className={style.int}/>
-            <img src={'https://e7.pngegg.com/pngimages/409/527/png-clipart-magnifying-glass-computer-icons-magnifying-glass-glass-black.png'}/>
+
         </div>
     )
 }
