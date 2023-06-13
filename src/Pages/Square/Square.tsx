1ecf210760cfa30a3d472e81a51b96f293810eac
user <zumaevanvar36@gmail.com>
2023-06-13 17:08:59 +0300

    asd

--- src/Pages/Square/Square.tsx
+++ src/Pages/Square/Square.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from './style.module.css'
-
+import phone from '../../resourse/phone.jpeg'
 
 
 function Square(){
     return(
         <div className={style.container}>
         <div className={style.first}>
+            <img src={phone} className={style.phone}/>
             <div className={style.com}>47% complete</div>
 
         </div>
