203cd17dac0f688cbb2189c53fcaff2af6356d27
user <zumaevanvar36@gmail.com>
2023-06-13 17:24:35 +0300

    asd

--- src/Pages/Square/Square.tsx
+++ src/Pages/Square/Square.tsx
@@ -9,7 +9,12 @@ function Square(){
         <div className={style.first}>
             <img src={phone} className={style.phone}/>
             <div className={style.com}>47% complete</div>
+            <div className={style.app}>APP DESIGN</div>
 
+
+            <br/>
+            <div className={style.app}>App Design Task</div>
+            App Design Task
         </div>
 
             <div className={style.second}>
