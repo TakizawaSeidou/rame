eed6ff20110339c42e9c6d79ba8732c6526244c4
user <zumaevanvar36@gmail.com>
2023-06-13 14:20:34 +0300

    asd

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -6,9 +6,10 @@ import diamond from '../../resourse/diamond.jpeg'
 function Search(){
     return(
         <div className={style.container}>
+            <img src={'https://e7.pngegg.com/pngimages/409/527/png-clipart-magnifying-glass-computer-icons-magnifying-glass-glass-black.png'} className={style.ser}/>
+            <input placeholder={'Search  Anything'} className={style.int}/>
+            \<img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRh-eALKjfQWuZA7z4hxKKNlx2ZswSrIaeUrSywxstBc5mHrVti'} className={style.ser}/>
 
-            <input  placeholder={'Search  Anything'} className={style.int}/>
-            <img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRh-eALKjfQWuZA7z4hxKKNlx2ZswSrIaeUrSywxstBc5mHrVti'} className={style.ser}/>
         </div>
     )
 }
