4386c7fa8c6839a1eeeddf51a2516081c722ab10
user <zumaevanvar36@gmail.com>
2023-06-15 17:07:56 +0300

    asd

--- src/Screens/Second/TeamMember/TeamMember.tsx
+++ src/Screens/Second/TeamMember/TeamMember.tsx
@@ -4,7 +4,26 @@ import style from './style.module.css';
 function TeamMember(){
     return(
         <div className={style.container}>
-            TeamMember
+            <div className={style.teamMember}>
+                <a>Team Member</a>
+            </div>
+            <div className={style.member}>
+                <div className={style.member1}>
+                    <img src={'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}/>
+                </div>
+                <div className={style.member2}>
+                <img src={'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}/>
+                </div>
+                <div className={style.member2}>
+                    <img src={'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80'}/>
+                </div>
+                <div className={style.member2}>
+                    <img src={'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}/>
+                </div>
+                <div className={style.member2}>
+                <img src={'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}/>
+                </div>
+            </div>
         </div>
     )
 }
