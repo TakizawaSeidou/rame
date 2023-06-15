654d92ada806bb6039575d0a6b0769a12738a8a9
user <zumaevanvar36@gmail.com>
2023-06-15 18:22:12 +0300

    asd

--- src/Screens/First/Square2/Square2.tsx
+++ src/Screens/First/Square2/Square2.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import style from './style.module.css'
-
-
+import finger from '../../../resourse/finger.jpeg'
+import ball from '../../../resourse/ball.jpeg'
 
 function Square(){
     return(
         <div className={style.container}>
             <div className={style.first}>
-
+            <img src={finger}/>
+                CLIENT
+                <br/>
+                Awe studio des...
 
             </div>
-            <div className={style.second}></div>
+
+            <div className={style.second}>
+            <img src={ball}/>
+                DRIBBBLE
+                <br/>
+                Create dribbble shot
+
+            </div>
         </div>
 
     )
